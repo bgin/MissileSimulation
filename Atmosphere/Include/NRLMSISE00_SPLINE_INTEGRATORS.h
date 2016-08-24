812d5bc4b17c9cb03d81cc4c8898b99167b76fc3
Bernard <beniekg@gmail.com>
2016-08-24 18:24:18 +0200

    Added __restrict keyword

--- Atmosphere/Include/NRLMSISE00_SPLINE_INTEGRATORS.h
+++ Atmosphere/Include/NRLMSISE00_SPLINE_INTEGRATORS.h
@@ -12,8 +12,10 @@ namespace  atmosphere {
 	/* ------------------------------------------------------------------- */
 	template<typename T> struct SPLINE_INTEGRATOR {
                
-		static std::enable_if<ad::is_single_precision<T>::value || ad::is_double_precision<T>::value, void>::type
-		splinint(_In_ const T*, _In_ const T*, _In_ const T*, _In_ const int, _In_ const T, _Out_ T*)noexcept(false);
+		static std::enable_if<ad::is_single_precision<T>::value ||
+		                               ad::is_double_precision<T>::value, void>::type
+	 	                                splinint(_In_ const T*__restrict, _In_ const T* __restrict, _In_ const T* __restrict, 
+	 	                                                        _In_ const int, _In_ const T, _Out_ T*__restrict)noexcept(false);
 
 	};
 
@@ -22,8 +24,10 @@ namespace  atmosphere {
 	/* ------------------------------------------------------------------- */
 	template<typename T> struct SPLINE_INTERPOLATOR {
 
-		static std::enable_if<ad::is_single_precision<T>::value || ad::is_double_precision<T>::value, void>::type
-		splinterp(_In_ const T*, _In_ const T*, _In_ const T*, _In_ const int, _In_ const T, _Out_  T*)noexcept(false);
+		static std::enable_if<ad::is_single_precision<T>::value || 
+		                    ad::is_double_precision<T>::value, void>::type
+	                          	splinterp(_In_ const T* __restrict, _In_ const T* __restrict, 
+		                            _In_ const T* __restrict, _In_ const int, _In_ const T, _Out_  T*__restrict)noexcept(false);
 	};
 
 	/* ------------------------------------------------------------------- */
@@ -31,8 +35,10 @@ namespace  atmosphere {
 	/* ------------------------------------------------------------------- */
 	template<typename T> struct SPLINE_CUBIC_DERIV {
 
-		static std::enable_if<ad::is_single_precision<T>::value || ad::is_double_precision<T>::value, void>::type
-		splinderiv(_In_ const T*, _In_ const T*, _In_ const int, _In_ const T, _In_ const T, _Out_ T*)noexcept(false);
+		static std::enable_if<ad::is_single_precision<T>::value || 
+		                      ad::is_double_precision<T>::value, void>::type
+	                          	splinderiv(_In_ const T*__restrict, _In_ const T*__restrict, _In_ const int, 
+		                                        _In_ const T, _In_ const T, _Out_ T*__restrict)noexcept(false);
 	};
 #include "NRLMSISE00_SPLINE_INTEGRATORS.inl"
 }
